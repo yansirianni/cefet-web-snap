e91df3a69093b422ff6a4bf8f4e9acbd1a167659
Yan Victor <yanvictorsa@yahoo.com.br>
2021-07-04 11:04:37 -0300

    Allow user change image - Challenge

--- script/controles-foto-anotada.js
+++ script/controles-foto-anotada.js
@@ -1,6 +1,35 @@
+//common
+
 const filtroEl = document.querySelector('#filtro-da-foto');
+const seletorImagemEl = document.querySelector('#imagem');
+const imagemEl = document.querySelector('.foto-anotada > img');
+
+//Exercice 4
 
 filtroEl.addEventListener('input', e => {
-    let imagemEl = document.querySelector('.foto-anotada > img');
+    
     imagemEl.style.filter = filtroEl.value;
 })
+
+//challenge
+
+seletorImagemEl.addEventListener('change', (event) => {    
+    const fileList = event.target.files;   
+
+    readImage(fileList[0], imagemEl);
+  });
+
+function readImage(file, imageEl){
+
+// Check if the file is an image.
+if (file.type && !file.type.startsWith('image/')) {
+    console.log('File is not an image.', file.type, file);  
+    return; 
+}    
+
+const reader = new FileReader();
+reader.addEventListener('load', (event) => {
+    imageEl.src = event.target.result;
+});
+reader.readAsDataURL(file);
+}
